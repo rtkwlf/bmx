d942c2867675be94536840836084a9ac2f360fe3
Jonathan Beverly <jonathan.beverly@arcticwolf.com>
2020-12-31 14:57:47 -0500

    Replace version cmd with cobra --version

--- cmd/bmx/version.go
+++ cmd/bmx/version.go
@@ -16,22 +16,9 @@ limitations under the License.
 
 package main
 
-import (
-	"fmt"
-
-	"github.com/spf13/cobra"
-)
-
 var version string
 
 func init() {
-	rootCmd.AddCommand(versionCmd)
-}
-
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print BMX version and exit",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("BMX version %s\n", version)
-	},
+	rootCmd.InitDefaultVersionFlag()
+	rootCmd.Version = version
 }
