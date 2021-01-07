0db47a60711aaba88b4981f5227d2ebcbae1460b
Jonathan Beverly <jonathan.beverly@arcticwolf.com>
2021-01-07 13:13:08 -0500

    variable name fix

--- cmd/bmx/write.go
+++ cmd/bmx/write.go
@@ -81,8 +81,8 @@ func MergeWriteCmdOptions(uc config.UserConfig, wc bmx.WriteCmdOptions) bmx.Writ
 	if wc.Role == "" {
 		wc.Role = uc.Role
 	}
-	if pc.Factor == "" {
-		pc.Factor = uc.Factor
+	if wc.Factor == "" {
+		wc.Factor = uc.Factor
 	}
 
 	return wc
