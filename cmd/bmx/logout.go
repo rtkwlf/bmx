9efffc521b49f3facf4e3cb6e0afe2a2fdda350a
Jonathan Beverly <jonathan.beverly@arcticwolf.com>
2021-08-16 09:58:16 -0400

    Command for logging out

--- cmd/bmx/logout.go
+++ cmd/bmx/logout.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/rtkwlf/bmx/config"
@@ -12,15 +11,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var logoutOptions = bmx.LoginCmdOptions{}
+var logoutOptions = bmx.LogoutCmdOptions{}
 
 func init() {
 	logoutCmd.Flags().StringVar(&logoutOptions.Org, "org", "", "the okta org api to target")
-	logoutCmd.Flags().StringVar(&logoutOptions.User, "user", "", "the user to authenticate with")
-	logoutCmd.Flags().StringVar(&logoutOptions.Account, "account", "", "the account name to auth against")
-	logoutCmd.Flags().StringVar(&logoutOptions.Role, "role", "", "the desired role to assume")
-	logoutCmd.Flags().BoolVar(&logoutOptions.NoMask, "nomask", false, "set to not mask the password. this helps with debugging.")
-
 	if userConfig.Org == "" {
 		logoutCmd.MarkFlagRequired("org")
 	}
@@ -30,37 +24,22 @@ func init() {
 
 var logoutCmd = &cobra.Command{
 	Use:   "logout",
-	Short: "Revokes the session",
-	Long:  `Forces revocation of the session`,
+	Short: "Revokes all Okta sessions",
+	Long:  `Forces revocation of the Okta sessions`,
 	Run: func(cmd *cobra.Command, args []string) {
 		mergedOptions := mergeLogoutOptions(userConfig, logoutOptions)
-
 		oktaClient, err := okta.NewOktaClient(mergedOptions.Org, consolerw)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		response := bmx.Login(oktaClient, consolerw, mergedOptions)
-		fmt.Println(response)
+		bmx.Logout(oktaClient)
 	},
 }
 
-func mergeLogoutOptions(uc config.UserConfig, pc bmx.LoginCmdOptions) bmx.LoginCmdOptions {
+func mergeLogoutOptions(uc config.UserConfig, pc bmx.LogoutCmdOptions) bmx.LogoutCmdOptions {
 	if pc.Org == "" {
 		pc.Org = uc.Org
 	}
-	if pc.User == "" {
-		pc.User = uc.User
-	}
-	if pc.Account == "" {
-		pc.Account = uc.Account
-	}
-	if pc.Role == "" {
-		pc.Role = uc.Role
-	}
-	if pc.Factor == "" {
-		pc.Factor = uc.Factor
-	}
-
 	return pc
 }
