9eb0d3c28b19bb407a21ec89232b4214bec28c5c
Jonathan Beverly <jonathan.beverly@arcticwolf.com>
2021-08-16 10:07:48 -0400

    run formatters

--- login.go
+++ login.go
@@ -22,7 +22,7 @@ type LoginCmdOptions struct {
 }
 
 type LogoutCmdOptions struct {
-	Org      string
+	Org string
 }
 
 func GetUserInfoFromLoginCmdOptions(loginOptions LoginCmdOptions) serviceProviders.UserInfo {
